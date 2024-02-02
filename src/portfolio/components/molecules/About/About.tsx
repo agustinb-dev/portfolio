f87b28356f8028c53078286795e30d3267e567ac
Agustin <lyricquinn27@gmail.com>
2024-02-02 12:01:37 -0300

    changed hero button font size

--- src/portfolio/components/molecules/About/About.tsx
+++ src/portfolio/components/molecules/About/About.tsx
@@ -37,22 +37,23 @@ export function About(properties: AboutProperties) {
 							<Typography Component={"p"} variant={"normal"} size={"large"}>
 								Hi!, my name is Agustin Barcia, a self-taught with 26 years old who is passionate about the world of web development.
 								<br/><br/>
-								My orientation is Front-End, focused more than anything on Typescript.
+								My orientation is Front-End, focused more than anything on React and Typescript.
 								I&#39;m motivated to continue learning and gaining experience in the professional
 								field.<br/><br/>
-								I believe that being a Front-End developer means not being afraid of challenges and
+								I believe that being a developer means not being afraid of challenges and
 								have the ability to continually learn to tackle new projects more efficiently.
 							</Typography>
-						) : (<Typography Component={"p"} variant={"normal"} size={"large"}>
-							Mi nombre es Agustin Barcia, tengo 26 años, soy un estudiante en la carrera de ingeniería en
-							computación y autodidacta,
-							apasionado por el mundo del desarrollo mobile y web.<br/><br/>
-							Mi orientación es Full-Stack, centrado más que nada en Python y JavaScript.
-							Estoy motivado a seguir aprendiendo y adquiriendo experiencia en el campo
-							profesional.<br/><br/>
-							Creo que ser un desarrollador Full Stack significa no tener miedo a los desafíos y tener la
-							capacidad de aprender continuamente para abordar nuevos proyectos con mayor eficiencia.
-						</Typography>)}
+						) : (
+							<Typography Component={"p"} variant={"normal"} size={"large"}>
+								Mi nombre es Agustin Barcia, tengo 26 años y soy un autodidacta apasionado en por el mundo del desarrollo web.<br/><br/>
+								Mi orientación es Front-End, centrado más que nada en React y Typescript.
+
+								Estoy motivado a seguir aprendiendo y adquiriendo experiencia en el campo profesional.<br/><br/>
+
+								Creo que ser un desarrollador significa no tener miedo a los desafíos y tener la
+								capacidad de aprender continuamente para abordar nuevos proyectos con mayor eficiencia.
+							</Typography>
+						)}
 					</div>
 					<hr className={styles.horizontalLine}/>
 					<div className={styles.technologiesContainer}>
