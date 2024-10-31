7af0c4e2efc82c5cc259fcafe3fca1d1cc1bf42d
Agustín Barcia <agustinbarcia.dev@gmail.com>
2024-10-31 08:32:28 -0300

    changed page title and removed  personal instagram from socials

--- src/portfolio/components/molecules/SocialList/SocialList.tsx
+++ src/portfolio/components/molecules/SocialList/SocialList.tsx
@@ -3,7 +3,6 @@ import * as styles from "./SocialList.module.scss";
 import { AnchorIconButton } from "../../atoms";
 import { FaGithub } from "@react-icons/all-files/fa/FaGithub";
 import { FaLinkedin } from "@react-icons/all-files/fa/FaLinkedin";
-import { AiFillInstagram } from "@react-icons/all-files/ai/AiFillInstagram";
 
 export function SocialList() {
 	return (
@@ -22,14 +21,6 @@ export function SocialList() {
 						<FaLinkedin size={20}/>
 					</AnchorIconButton>
 					<span>Linkedin</span>
-				</div>
-			</li>
-			<li key={"instagram"}>
-				<div className={styles.socialBox}>
-					<AnchorIconButton href={"https://www.instagram.com/eternallydie/"}>
-						<AiFillInstagram size={20}/>
-					</AnchorIconButton>
-					<span>Instagram</span>
 				</div>
 			</li>
 		</ul>
